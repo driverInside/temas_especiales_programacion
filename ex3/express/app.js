18a9c97765a08d24eaaf3e6f4ba8e90054f66c03
Hiram E. Perez <hiram.eps@gmail.com>
2023-03-06 07:35:22 -0600

    add request file

--- ex3/express/app.js
+++ ex3/express/app.js
@@ -22,6 +22,33 @@ app.put('/foo', (req, res) => {
   })
 });
 
+app.get('/random', (_, res) => {
+  res.send({
+    number: getRandomIntInclusive(1, 10)
+  });
+});
+
+app.post('/fib/:nth', (req, res) => {
+  const nth = req.params.nth; // const { nth } = req.params;
+  const result = [];
+
+  for (let i = 0; i <= nth; i++) {
+    result.push(fib(i));
+  }
+
+  res.send({ result });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running at PORT :${PORT}`);
 });
+
+function fib(nth) {
+  return nth < 2 ? 1: fib (nth - 1) + fib (nth - 2);
+}
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
